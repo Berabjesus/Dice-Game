00703f3a1a72ac87a50fb33d3df1e63326f5d09c
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-11-22 22:28:50 +0300

    ADD player containers

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,13 @@
 window.onload = () => {
-  let boardContainer = document.querySelector('.board-container')
-  let playerOneBoard = document.createElement('div')
-  let playerTwoBoard = document.createElement('div')
-  playerOneBoard.classList.add('col-6', 'boarder')
-  playerTwoBoard.classList.add('col-6', 'boarder')
+  // let boardContainer = document.querySelector('.board-container')
+
+  // let board = document.createElement('div')
+  // board.classList.add('col-6', 'd-flex', 'flex-column','align-items-center', 'py-4' , 'border-dash')
+
+  // let playerName = document.createElement('h1')
+  // playerName.innerText = 'Player'
+  // board.appendChild(playerName)
+
+  // boardContainer.appendChild(board)
+  // boardContainer.appendChild(board.cloneNode())
 }
