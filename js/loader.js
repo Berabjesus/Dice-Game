6d63b40b6bf49227319b24380974d9f79faf8338
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-11-22 21:29:22 +0300

    ADD javascript file with onload function

---
+++ js/loader.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  let boardContainer = document.querySelector('.board-container')
+  let playerOneBoard = document.createElement('div')
+  let playerTwoBoard = document.createElement('div')
+  playerOneBoard.classList.add('col-6', 'boarder')
+  playerTwoBoard.classList.add('col-6', 'boarder')
+}
