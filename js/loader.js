86548a229f320e0ab007d85c8b9615f9818468ce
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-11-23 15:42:48 +0300

    ADD dice dots with javascript

--- js/loader.js
+++ js/loader.js
@@ -3,15 +3,21 @@ window.onload = () => {
   let dot = document.createElement('div')
   dot.classList.add('dot')
 
+  let sides = document.querySelectorAll('.face')
   let diceDots = [
     ['e'], ['c', 'g'],['c','e', 'g'],['a','c','g','i'],['a','c','e','g','i'],['a','c','d','f','g','i']
   ]
 
-  diceDots.forEach(element => {
-    
-  });
-    dot.style.gridArea = 'e'
-    faceOne.appendChild(dot.cloneNode())  
-    dot.style.gridArea = 'a'
-    faceOne.appendChild(dot.cloneNode())  
+  // console.log(sides[0])
+  for (let i = 0; i < sides.length; i++) {
+    for (let j = 0; j < diceDots[i].length; j++) {
+      dot.style.gridArea = diceDots[i][j]
+      sides[i].appendChild(dot.cloneNode()) 
+    }    
+  }
+
+    // dot.style.gridArea = 'e'
+    // faceOne.appendChild(dot.cloneNode())  
+    // dot.style.gridArea = 'a'
+    // faceOne.appendChild(dot.cloneNode())  
 }
