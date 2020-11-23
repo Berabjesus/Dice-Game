bcef6194ca84eb05bda7fae9e58dd0e7f9ec6325
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-11-23 22:18:52 +0300

    ADD reset function to reset the board when new game button is clicked

--- js/boardLogic.js
+++ js/boardLogic.js
@@ -19,10 +19,24 @@ function updateTotalScore() {
   updatePlayer(player)
 }
 
-function updatePlayer(player) {
+function updatePlayer(player, reset = false) {
   player.textContent = 0
   player.parentElement.parentElement.classList.remove('active-player')
-  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+  if (reset == false) {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+  }
   document.querySelector('#current-' + activePlayer).parentElement.parentElement.classList.add('active-player')
+}
 
+function reset() {
+  activePlayer = 0
+  let player = document.querySelector('#current-' + activePlayer)
+  updatePlayer(player, true)
+  let allTotalScores = document.getElementsByClassName('total-count')
+
+  for (let i = 0; i < allTotalScores.length; i++) {
+    allTotalScores[i].textContent = 0
+  }
+
+  updateDice(document.getElementById('dice'), 1)
 }
