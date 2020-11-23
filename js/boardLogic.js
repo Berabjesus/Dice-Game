7e530cc817f5f270c69e40fcbd571848d528b2b2
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-11-23 22:03:09 +0300

    ADD update total score function to update the total score once the user hits the hold button

--- js/boardLogic.js
+++ js/boardLogic.js
@@ -3,12 +3,26 @@ function updateCurrentScore(randomNumber) {
   let currentScore = player.textContent
 
   if (randomNumber === 1) {
-    player.textContent = 0
-    player.parentElement.parentElement.classList.remove('active-player')
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-    document.querySelector('#current-' + activePlayer).parentElement.parentElement.classList.add('active-player')
-
+    updatePlayer(player)
   } else {
     player.textContent = parseInt(currentScore) + parseInt (randomNumber)
   }
+}
+
+
+function updateTotalScore() {
+  let player = document.querySelector('#current-' + activePlayer)
+  let currentScore = player.textContent
+  let totalScore = document.querySelector('#total-' + activePlayer)
+
+  totalScore.textContent = parseInt(totalScore.textContent) + parseInt(currentScore)
+  updatePlayer(player)
+}
+
+function updatePlayer(player) {
+  player.textContent = 0
+  player.parentElement.parentElement.classList.remove('active-player')
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+  document.querySelector('#current-' + activePlayer).parentElement.parentElement.classList.add('active-player')
+
 }
